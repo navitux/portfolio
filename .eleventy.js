0ab52017cf8a5660e2e12fb7567caeeded598c25
Ivan Robles <navitux@disroot.org>
2023-01-02 01:54:06 -0600

    drafts and tests: css style has been applied, plugin used

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,21 @@
+const directoryOutputPlugin = require("@11ty/eleventy-plugin-directory-output");
+
 module.exports = function(eleventyConfig) {
-  // Return your Object options:
+  // Passthrough dirs:
   eleventyConfig.addPassthroughCopy("src/img");
+  eleventyConfig.addPassthroughCopy("src/static");
+  // Plugins
+  eleventyConfig.addPlugin(directoryOutputPlugin, {
+    // Customize columns
+    columns: {
+      filesize: true, // Use `false` to disable
+      benchmark: true, // Use `false` to disable
+    },
+
+    // Will show in yellow if greater than this number of bytes
+    warningFileSize: 400 * 1000,
+  });
+
   return {
     dir: {
       input: "src",
