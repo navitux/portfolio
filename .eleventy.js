63fd42e1a68056aa14be4b952511949cafab0789
Ivan Robles <navitux@disroot.org>
2023-01-02 09:44:04 -0600

    drafts n tests: html files minified and plugin added

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,20 @@
 const directoryOutputPlugin = require("@11ty/eleventy-plugin-directory-output");
+const eleventyPluginFilesMinifier = require("@sherby/eleventy-plugin-files-minifier");
 
-module.exports = function(eleventyConfig) {
+module.exports = (eleventyConfig) => {
   // Passthrough dirs:
   eleventyConfig.addPassthroughCopy("./src/img");
   eleventyConfig.addPassthroughCopy("./src/static");
   eleventyConfig.addWatchTarget("./src/static/");
-  // Plugins
+  // output directory
   eleventyConfig.addPlugin(directoryOutputPlugin, {
-    // Customize columns
     columns: {
       filesize: true, // Use `false` to disable
       benchmark: true, // Use `false` to disable
     }});
-
+  // minifier
+  eleventyConfig.addPlugin(eleventyPluginFilesMinifier);
+  
   return {
     dir: {
       input: "src",
